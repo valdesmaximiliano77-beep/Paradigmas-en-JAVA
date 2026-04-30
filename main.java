dab1137b8753de8f71053ff56d72555452887bf6
sinonimoanonimo <andresigorolguingarrido@gmail.com>
2026-04-30 09:14:45 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import cl.unab.ptec102.ejemplopokemon.core.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+void main() {
+
+    Gimnasio elGimnasio = new Gimnasio();
+    PokemonVentana ventana = new PokemonVentana("Pokemon principal");
+
+
+    //Gimnasio elGimnasio = new Gimnasio();
+    //elGimnasio.comenzarBatalla();
+}
